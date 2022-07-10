fc97947888b77df827449ff4ac643520cc35b193
fukubaka0825 <st5818129@gmail.com>
2022-07-11 01:46:52 +0900

    一旦鳴きとreachみずに打牌判断だけにして、巷のエクセルではかるNAGA度の判定に完全に寄せる

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"io/ioutil"
 	"log"
 	"os"
@@ -28,11 +29,11 @@ const (
 type Result [][]StartOrAction
 
 type StartOrAction struct {
-	EndNum    int                 `json:"end_num"`
-	Info      Info                `json:"info"`
-	DahaiPred []float32           `json:"dahai_pred"`
-	Reach     float32             `json:"reach"`
-	Huro      map[int]map[int]int `json:"huro"`
+	EndNum    int                     `json:"end_num"`
+	Info      Info                    `json:"info"`
+	DahaiPred []float32               `json:"dahai_pred"`
+	Reach     float32                 `json:"reach"`
+	Huro      map[int]map[int]float32 `json:"huro"`
 }
 
 type Info struct {
@@ -61,15 +62,16 @@ func (t TehaiMap) changeTehai(actor int, tsumoPai, dahai string) {
 }
 
 type nagaJudge struct {
-	point        float32
-	judgeCount   int
-	nagaRate     float32
-	BadPlayCount int
+	judgeCount     int
+	matchCount     int
+	unMatchCount   int
+	BadPlayCount   int
+	minusValueList []float32
 }
 
 type nextHuroRecommend struct {
 	nextShouldHuro bool
-	rate           int
+	rate           float32
 	pattern        string
 }
 
@@ -80,20 +82,27 @@ type nextReachRecommend struct {
 
 type actorNagaMap map[int]*nagaJudge
 
-func (a actorNagaMap) culcNagaRate() {
+func (a actorNagaMap) culcNagaValue() {
 	for actor := 0; actor <= 3; actor++ {
-		a[actor].nagaRate = a[actor].point / float32(a[actor].judgeCount)
-		log.Printf("actor: %v, nagaJudgeCount: %v, point: %v,nagaRate: %v, BadPlayCount: %v", actor, a[actor].judgeCount, a[actor].point, a[actor].nagaRate, a[actor].BadPlayCount)
+		var minusSum float32
+		for _, v := range a[actor].minusValueList {
+			minusSum += v
+		}
+		nagaValue := 100 - minusSum/float32(len(a[actor].minusValueList)*100)
+		log.Printf("actor: %v,nagaJudgeCount: %v,matchCount: %v,unMatchCount: %v,nagaValue: %v, BadPlayCount: %v", actor, a[actor].judgeCount, a[actor].matchCount, a[actor].unMatchCount, nagaValue, a[actor].BadPlayCount)
 	}
 }
 
 func main() {
 	actorNagaMap := actorNagaMap{
-		0: {0, 0, 0, 0},
-		1: {0, 0, 0, 0},
-		2: {0, 0, 0, 0},
-		3: {0, 0, 0, 0},
+		0: {0, 0, 0, 0, []float32{}},
+		1: {0, 0, 0, 0, []float32{}},
+		2: {0, 0, 0, 0, []float32{}},
+		3: {0, 0, 0, 0, []float32{}},
 	}
+	var selectActor int
+	flag.IntVar(&selectActor, "actor", 0, "log出力したいactorを選択")
+	flag.Parse()
 	realPaiNagaPaiIndexMap := getRealPaiNagaPaiIndexMap()
 	var result Result
 	playerTehaiMap := make(TehaiMap)
@@ -129,75 +138,135 @@ func main() {
 			actionIndex++
 			action := result[kyokuIndex][actionIndex]
 			actor := action.Info.Msg.Actor
-			if nextReachRecommend[actor].nextShouldReach && action.Info.Msg.Type != TYPE_REACH {
-				actorNagaMap[actor].judgeCount++
-				if nextReachRecommend[actor].rate > BAD_PLAY_THRETHOLD {
-					// 悪手を出力してみる
-					log.Printf("!!BADPLAYYYYYY 鉄Reach!! actor: %v, bakaze: %v, kyoku: %v,leftHiNum: %v, reachPredRate: %v", actor, bakaze, kyoku, action.Info.Msg.LeftHaiNum, nextReachRecommend[actor].rate)
-					actorNagaMap[actor].BadPlayCount++
-				}
-				nextReachRecommend[actor].nextShouldReach = false
-				nextReachRecommend[actor].rate = 0
-			}
-			if !nextReachRecommend[actor].nextShouldReach && action.Info.Msg.Type == TYPE_REACH {
-				actorNagaMap[actor].judgeCount++
-				log.Printf("ダマ推奨 actor: %v, bakaze: %v, kyoku: %v,leftHiNum: %v, reachPredRate: %v", actor, bakaze, kyoku, action.Info.Msg.LeftHaiNum, nextReachRecommend[actor].rate)
-				nextReachRecommend[actor].rate = 0
-			}
-			if nextHuroRecommend[actor].nextShouldHuro && (action.Info.Msg.Type != TYPE_PON && action.Info.Msg.Type != TYPE_CHI) {
-				actorNagaMap[actor].judgeCount++
-				if nextHuroRecommend[actor].rate > BAD_PLAY_THRETHOLD {
-					// 悪手を出力してみる
-					log.Printf("!!BADPLAYYYYYY 鉄huro!! actor: %v, bakaze: %v, kyoku: %v,leftHiNum: %v, playerChoice: %v, playerChoicePredRate: %v nagaChoice: %v nagaChoicePredRate: %v pattern: %v", actor, bakaze, kyoku, action.Info.Msg.LeftHaiNum, "huroせず", 0, action.Info.Msg.PredDahai, nextHuroRecommend[actor].rate, nextHuroRecommend[actor].pattern)
-					actorNagaMap[actor].BadPlayCount++
-				}
-				nextHuroRecommend[actor].nextShouldHuro = false
-				nextHuroRecommend[actor].rate = 0
-				nextHuroRecommend[actor].pattern = ""
-			}
-			if !nextHuroRecommend[actor].nextShouldHuro && (action.Info.Msg.Type == TYPE_PON || action.Info.Msg.Type == TYPE_CHI) {
-				actorNagaMap[actor].judgeCount++
-				log.Printf("huroしない推奨 actor: %v, bakaze: %v, kyoku: %v,leftHiNum: %v, huroPredRate: %v, huroPredPattern: %v", actor, bakaze, kyoku, action.Info.Msg.LeftHaiNum, nextHuroRecommend[actor].rate, nextHuroRecommend[actor].pattern)
 
-				nextHuroRecommend[actor].nextShouldHuro = false
-				nextHuroRecommend[actor].rate = 0
-				nextHuroRecommend[actor].pattern = ""
-			}
-			if action.Info.Msg.Type == TYPE_TSUMO {
+			//// リーチ判断判定ゾーン
+			//if nextReachRecommend[actor].nextShouldReach && action.Info.Msg.Type == TYPE_REACH {
+			//	actorNagaMap[actor].judgeCount++
+			//	actorNagaMap[actor].matchCount++
+			//	actorNagaMap[actor].minusValueList = append(actorNagaMap[actor].minusValueList, 0)
+			//	nextReachRecommend[actor].nextShouldReach = false
+			//	nextReachRecommend[actor].rate = 0
+			//}
+			//if nextReachRecommend[actor].nextShouldReach && action.Info.Msg.Type != TYPE_REACH {
+			//	actorNagaMap[actor].judgeCount++
+			//	actorNagaMap[actor].unMatchCount++
+			//	actorNagaMap[actor].minusValueList = append(actorNagaMap[actor].minusValueList, nextReachRecommend[actor].rate)
+			//	if nextReachRecommend[actor].rate > BAD_PLAY_THRETHOLD {
+			//		// 悪手を出力してみる
+			//		if actor == selectActor {
+			//			log.Printf("!!BADPLAYYYYYY 鉄Reach!! actor: %v, bakaze: %v, kyoku: %v,leftHiNum: %v, reachPredRate: %v", actor, bakaze, kyoku, action.Info.Msg.LeftHaiNum, nextReachRecommend[actor].rate)
+			//		}
+			//		actorNagaMap[actor].BadPlayCount++
+			//		nextReachRecommend[actor].nextShouldReach = false
+			//		nextReachRecommend[actor].rate = 0
+			//	} else {
+			//		if actor == selectActor {
+			//			log.Printf("Reach推奨 actor: %v, bakaze: %v, kyoku: %v,leftHiNum: %v, reachPredRate: %v", actor, bakaze, kyoku, action.Info.Msg.LeftHaiNum, nextReachRecommend[actor].rate)
+			//		}
+			//		nextReachRecommend[actor].nextShouldReach = false
+			//		nextReachRecommend[actor].rate = 0
+			//	}
+			//}
+			//if !nextReachRecommend[actor].nextShouldReach && action.Info.Msg.Type == TYPE_REACH {
+			//	actorNagaMap[actor].judgeCount++
+			//	actorNagaMap[actor].unMatchCount++
+			//	if actor == selectActor {
+			//		log.Printf("ダマ推奨 actor: %v, bakaze: %v, kyoku: %v,leftHiNum: %v, reachPredRate: %v", actor, bakaze, kyoku, action.Info.Msg.LeftHaiNum, nextReachRecommend[actor].rate)
+			//	}
+			//	actorNagaMap[actor].minusValueList = append(actorNagaMap[actor].minusValueList, RECOMMEND_THRETHOLD-nextReachRecommend[actor].rate)
+			//	nextReachRecommend[actor].nextShouldReach = false
+			//	nextReachRecommend[actor].rate = 0
+			//}
+
+			//// 鳴き判断判定ゾーン
+			//if nextHuroRecommend[actor].nextShouldHuro && (action.Info.Msg.Type == TYPE_PON || action.Info.Msg.Type == TYPE_CHI) {
+			//	actorNagaMap[actor].judgeCount++
+			//	actorNagaMap[actor].matchCount++
+			//	actorNagaMap[actor].minusValueList = append(actorNagaMap[actor].minusValueList, 0)
+			//	nextHuroRecommend[actor].nextShouldHuro = false
+			//	nextHuroRecommend[actor].rate = 0
+			//	nextHuroRecommend[actor].pattern = ""
+			//}
+			//if nextHuroRecommend[actor].nextShouldHuro && action.Info.Msg.Type != TYPE_PON && action.Info.Msg.Type != TYPE_CHI {
+			//	actorNagaMap[actor].judgeCount++
+			//	actorNagaMap[actor].unMatchCount++
+			//	actorNagaMap[actor].minusValueList = append(actorNagaMap[actor].minusValueList, nextHuroRecommend[actor].rate)
+			//	if nextHuroRecommend[actor].rate > BAD_PLAY_THRETHOLD {
+			//		// 悪手を出力してみる
+			//		if actor == selectActor {
+			//			log.Printf("!!BADPLAYYYYYY 鉄huro!! actor: %v, bakaze: %v, kyoku: %v,leftHiNum: %v, playerChoice: %v, playerChoicePredRate: %v nagaChoice: %v nagaChoicePredRate: %v pattern: %v", actor, bakaze, kyoku, action.Info.Msg.LeftHaiNum, "huroせず", 0, action.Info.Msg.PredDahai, nextHuroRecommend[actor].rate, nextHuroRecommend[actor].pattern)
+			//		}
+			//		actorNagaMap[actor].BadPlayCount++
+			//		nextHuroRecommend[actor].nextShouldHuro = false
+			//		nextHuroRecommend[actor].rate = 0
+			//		nextHuroRecommend[actor].pattern = ""
+			//	} else {
+			//		if actor == selectActor {
+			//			log.Printf("huro推奨 actor: %v, bakaze: %v, kyoku: %v,leftHiNum: %v, playerChoice: %v, playerChoicePredRate: %v nagaChoice: %v nagaChoicePredRate: %v pattern: %v", actor, bakaze, kyoku, action.Info.Msg.LeftHaiNum, "huroせず", 0, action.Info.Msg.PredDahai, nextHuroRecommend[actor].rate, nextHuroRecommend[actor].pattern)
+			//		}
+			//		nextHuroRecommend[actor].nextShouldHuro = false
+			//		nextHuroRecommend[actor].rate = 0
+			//		nextHuroRecommend[actor].pattern = ""
+			//	}
+			//}
+			//if !nextHuroRecommend[actor].nextShouldHuro && (action.Info.Msg.Type == TYPE_PON || action.Info.Msg.Type == TYPE_CHI) {
+			//	actorNagaMap[actor].judgeCount++
+			//	actorNagaMap[actor].unMatchCount++
+			//	if actor == selectActor {
+			//		log.Printf("huroしない推奨 actor: %v, bakaze: %v, kyoku: %v,leftHiNum: %v, huroPredRate: %v, huroPredPattern: %v", actor, bakaze, kyoku, action.Info.Msg.LeftHaiNum, nextHuroRecommend[actor].rate, nextHuroRecommend[actor].pattern)
+			//	}
+			//	actorNagaMap[actor].minusValueList = append(actorNagaMap[actor].minusValueList, RECOMMEND_THRETHOLD-nextHuroRecommend[actor].rate)
+			//	nextHuroRecommend[actor].nextShouldHuro = false
+			//	nextHuroRecommend[actor].rate = 0
+			//	nextHuroRecommend[actor].pattern = ""
+			//}
+
+			// 打牌判定ゾーン
+			if action.Info.Msg.Type == TYPE_TSUMO || action.Info.Msg.Type == TYPE_PON || action.Info.Msg.Type == TYPE_CHI {
 				if action.Reach != 0 {
 					nextReachRecommend[actor].rate = action.Reach
 					if action.Reach > RECOMMEND_THRETHOLD {
 						nextReachRecommend[actor].nextShouldReach = true
 					}
 				}
-				actorNagaMap[actor].judgeCount++
 				// 手配交換
 				// playerTehaiMap.changeTehai(actor, action.Info.Msg.Pai, action.Info.Msg.RealDahai)
+				if action.Info.Msg.RealDahai == "" || action.Info.Msg.PredDahai == "" {
+					continue
+				}
 				// NAG推奨打廃できてない場合は、推奨レート次第で加算ポイントを変える
 				if action.Info.Msg.RealDahai != action.Info.Msg.PredDahai {
 					realDahaiNagaPredRate := action.DahaiPred[realPaiNagaPaiIndexMap[action.Info.Msg.RealDahai]]
 					predDahaiNagaPredRate := action.DahaiPred[realPaiNagaPaiIndexMap[action.Info.Msg.PredDahai]]
 					if predDahaiNagaPredRate == 0 {
-						actorNagaMap[actor].point++
 						continue
 					}
+					actorNagaMap[actor].judgeCount++
+					actorNagaMap[actor].unMatchCount++
 					if predDahaiNagaPredRate-realDahaiNagaPredRate > BAD_PLAY_THRETHOLD {
 						// 悪手を出力してみる
-						log.Printf("!!BADPLAYYYYYY 打牌選択ミス!! actor: %v, bakaze: %v, kyoku: %v,leftHiNum: %v, playerChoice: %v, playerChoicePredRate: %v nagaChoice: %v nagaChoicePredRate: %v", actor, bakaze, kyoku, action.Info.Msg.LeftHaiNum, action.Info.Msg.RealDahai, realDahaiNagaPredRate, action.Info.Msg.PredDahai, predDahaiNagaPredRate)
+						if actor == selectActor {
+							log.Printf("!!BADPLAYYYYYY 打牌選択ミス!! actor: %v, bakaze: %v, kyoku: %v,leftHiNum: %v, playerChoice: %v, playerChoicePredRate: %v nagaChoice: %v nagaChoicePredRate: %v", actor, bakaze, kyoku, action.Info.Msg.LeftHaiNum, action.Info.Msg.RealDahai, realDahaiNagaPredRate, action.Info.Msg.PredDahai, predDahaiNagaPredRate)
+						}
 						actorNagaMap[actor].BadPlayCount++
-						actorNagaMap[actor].point = actorNagaMap[actor].point + realDahaiNagaPredRate/predDahaiNagaPredRate
+						actorNagaMap[actor].minusValueList = append(actorNagaMap[actor].minusValueList, predDahaiNagaPredRate-realDahaiNagaPredRate)
 						continue
 					}
-					log.Printf("打廃推奨 actor: %v, bakaze: %v, kyoku: %v,leftHiNum: %v, playerChoice: %v, playerChoicePredRate: %v nagaChoice: %v nagaChoicePredRate: %v", actor, bakaze, kyoku, action.Info.Msg.LeftHaiNum, action.Info.Msg.RealDahai, realDahaiNagaPredRate, action.Info.Msg.PredDahai, predDahaiNagaPredRate)
-					actorNagaMap[actor].point = actorNagaMap[actor].point + realDahaiNagaPredRate/predDahaiNagaPredRate
+					if actor == selectActor {
+						log.Printf("打牌変更推奨 actor: %v, bakaze: %v, kyoku: %v,leftHiNum: %v, playerChoice: %v, playerChoicePredRate: %v nagaChoice: %v nagaChoicePredRate: %v", actor, bakaze, kyoku, action.Info.Msg.LeftHaiNum, action.Info.Msg.RealDahai, realDahaiNagaPredRate, action.Info.Msg.PredDahai, predDahaiNagaPredRate)
+					}
+					actorNagaMap[actor].minusValueList = append(actorNagaMap[actor].minusValueList, predDahaiNagaPredRate-realDahaiNagaPredRate)
 					continue
 				}
-				actorNagaMap[actor].point++
+				actorNagaMap[actor].matchCount++
+				actorNagaMap[actor].minusValueList = append(actorNagaMap[actor].minusValueList, 0)
+				actorNagaMap[actor].judgeCount++
+
 			}
 			if action.Info.Msg.Type == TYPE_DAHAI {
 				if action.Huro != nil {
 					for huroAct := 0; huroAct <= 3; huroAct++ {
-						huroRate, huroType := getBiggestIntAndIndex([]int{action.Huro[huroAct][1], action.Huro[huroAct][2], action.Huro[huroAct][3], action.Huro[huroAct][4], action.Huro[huroAct][5]})
+						huroRate, huroType := getBiggestFloatAndIndex([]float32{action.Huro[huroAct][1], action.Huro[huroAct][2], action.Huro[huroAct][3], action.Huro[huroAct][4], action.Huro[huroAct][5]})
 						nextHuroRecommend[huroAct].rate = huroRate
 						nextHuroRecommend[huroAct].pattern = strconv.Itoa(huroType)
 						if huroRate > RECOMMEND_THRETHOLD {
@@ -210,7 +279,7 @@ func main() {
 		}
 		kyokuIndex++
 	}
-	actorNagaMap.culcNagaRate()
+	actorNagaMap.culcNagaValue()
 }
 
 func getRealPaiNagaPaiIndexMap() map[string]int {
@@ -265,8 +334,8 @@ func remove(strings []string, search string) []string {
 	return result
 }
 
-func getBiggestIntAndIndex(list []int) (int, int) {
-	var biggestNumber int
+func getBiggestFloatAndIndex(list []float32) (float32, int) {
+	var biggestNumber float32
 	var biggestIndex int
 	for i, v := range list {
 		if v > biggestNumber {
